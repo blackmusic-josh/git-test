908048ad8dda0391683b0f025a24c0e04473876a
blackmusic-josh <josh960108@gmail.com>
2023-07-22 21:50:47 +0800

    Hello word

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('hello world!')
+import os
+from dotenv import load_dotenv
+path =  os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(path+"/ddd.env")
+
+print(os.getenv("password"),os.getenv('token'))
+print("good morning")
